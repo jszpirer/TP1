876fe6940a1cd5f0a0b4586afd0165cc8b4b5feb
jszpirer <jeanne.szpirer@ulb.be>
2021-02-24 15:30:39 +0100

    Initial commit

---
+++ Test/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizz(){
+        int nFizz = 5;
+        int nBuzz = 3;
+        int test = 10;
+        FizzBuzz fb = new FizzBuzz();
+
+    }
+
+
+}
---
+++ src/com/company/FizzBuzz.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FizzBuzz {
+
+    public String fizzBuzz(int nFizz, int nBuzz, int test){
+        if (test %)
+
+    }
+}
