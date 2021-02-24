09a6dff5d499f1f3196c9d075bae8c9f425b47e3
jszpirer <jeanne.szpirer@ulb.be>
2021-02-24 15:59:12 +0100

    TP terminé

--- Test/FizzBuzzTest.java
+++ Test/FizzBuzzTest.java
@@ -1,4 +1,6 @@
+import com.company.FizzBuzz;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
@@ -8,7 +10,31 @@ public class FizzBuzzTest {
         int nBuzz = 3;
         int test = 10;
         FizzBuzz fb = new FizzBuzz();
-
+        assertEquals("Fizz", fb.fizzBuzz(nFizz,nBuzz,test));
+    }
+    @Test
+    public void testBuzz(){
+        int nFizz = 5;
+        int nBuzz = 3;
+        int test = 9;
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("Buzz", fb.fizzBuzz(nFizz,nBuzz,test));
+    }
+    @Test
+    public void testFizzBuzz(){
+        int nFizz = 5;
+        int nBuzz = 3;
+        int test = 15;
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("FizzBuzz", fb.fizzBuzz(nFizz,nBuzz,test));
+    }
+    @Test
+    public void testOtherCases(){
+        int nFizz = 5;
+        int nBuzz = 3;
+        int test = 17;
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("17", fb.fizzBuzz(nFizz,nBuzz,test));
     }
 
 
--- src/com/company/FizzBuzz.java
+++ src/com/company/FizzBuzz.java
@@ -3,7 +3,15 @@ package com.company;
 public class FizzBuzz {
 
     public String fizzBuzz(int nFizz, int nBuzz, int test){
-        if (test %)
-
+        if (test % nBuzz==0 && test % nFizz==0){
+            return "FizzBuzz";
+        }
+        if (test % nFizz==0){
+            return "Fizz";
+        }
+        if (test % nBuzz==0){
+            return "Buzz";
+        }
+        return new String (String.valueOf(test));
     }
 }
